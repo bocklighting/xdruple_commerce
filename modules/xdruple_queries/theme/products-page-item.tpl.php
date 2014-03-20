087abaddf4242b818fa7f09c523391e4bc09c167
Andrey Mikheychik <a.mikheychik@gmail.com>
2014-03-20 14:24:25 -0400

    "Categories" and "Products by category" pages added (with minimal refactoring)

---
+++ modules/xdruple_queries/theme/products-page-item.tpl.php
@@ -0,0 +1,22 @@
+<div class="views-field views-field-field-product-image">
+  <span class="field-content"><?php print $img ?></span>
+</div>
+<div class="views-field views-field-title">
+  <span class="field-content"><?php print $title ?></span>
+</div>
+<div class="views-field views-field-subTitle">
+  <span class="field-content"><?php print $subtitle ?></span>
+</div>
+<div class="views-field views-field-commerce-price">
+  <span class="field-content"><?php print $price ?></span>
+</div>
+<div class="views-field views-field-nothing">
+  <span class="field-content">
+    <?php print $read_more ?>
+  </span>
+</div>
+<div class="views-field views-field-add-to-cart-form">
+  <span class="field-content">
+    <?php print $add_to_cart ?>
+  </span>
+</div>
