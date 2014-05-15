36ce2294f5809fc539b577496350df53b87155cf
Andrey Mikheychik <a.mikheychik@gmail.com>
2014-05-15 17:03:51 -0400

    #23448: Invoice and Order pages improved, fixed and themed

--- modules/xdruple_invoice/theme/invoice-page.tpl.php
+++ modules/xdruple_invoice/theme/invoice-page.tpl.php
@@ -1,43 +1,45 @@
-<?php if (!empty($logo)): ?>
-  <?php print $logo; ?>
-<?php endif; ?>
+<div class="xdruple-print xdruple-print-invoice">
+  <?php if (!empty($logo)): ?>
+    <?php print $logo; ?>
+  <?php endif; ?>
 
-<?php if (!empty($remit_to_address)): ?>
-  <?php print $remit_to_address ?>
-<?php endif; ?>
+  <?php if (!empty($remit_to_address)): ?>
+    <?php print $remit_to_address ?>
+  <?php endif; ?>
 
-<?php if (!empty($header_message)): ?>
-  <?php print $header_message ?>
-<?php endif; ?>
+  <?php if (!empty($header_table)): ?>
+    <?php print $header_table ?>
+  <?php endif; ?>
 
-<?php if (!empty($header_table)): ?>
-  <?php print $header_table ?>
-<?php endif; ?>
+  <?php if (!empty($header_message)): ?>
+    <?php print $header_message ?>
+  <?php endif; ?>
 
-<?php if (!empty($billing_info)): ?>
-  <?php print $billing_info ?>
-<?php endif; ?>
+  <?php if (!empty($billing_info)): ?>
+    <?php print $billing_info ?>
+  <?php endif; ?>
 
-<?php if (!empty($shipping_info)): ?>
-  <?php print $shipping_info ?>
-<?php endif; ?>
+  <?php if (!empty($shipping_info)): ?>
+    <?php print $shipping_info ?>
+  <?php endif; ?>
 
-<?php if (!empty($line_items_table)): ?>
-  <?php print $line_items_table ?>
-<?php endif; ?>
+  <?php if (!empty($tracking)): ?>
+    <?php print $tracking ?>
+  <?php endif; ?>
 
-<?php if (!empty($order_total)): ?>
-  <?php print $order_total ?>
-<?php endif; ?>
+  <?php if (!empty($line_items_table)): ?>
+    <?php print $line_items_table ?>
+  <?php endif; ?>
 
-<?php if (!empty($tracking)): ?>
-  <?php print $tracking ?>
-<?php endif; ?>
+  <?php if (!empty($order_total)): ?>
+    <?php print $order_total ?>
+  <?php endif; ?>
 
-<?php if (!empty($footer_message)): ?>
-  <?php print $footer_message ?>
-<?php endif; ?>
+  <?php if (!empty($footer_message)): ?>
+    <?php print $footer_message ?>
+  <?php endif; ?>
 
-<?php if (!empty($notes)): ?>
-  <?php print $notes ?>
-<?php endif; ?>
+  <?php if (!empty($notes)): ?>
+    <?php print $notes ?>
+  <?php endif; ?>
+</div>
