b284ba1624f529a763e84c59fbcea2d762fed9f0
Andrey Mikheychik <a.mikheychik@gmail.com>
2014-11-03 16:12:57 -0500

    Subcategories handling added

--- modules/xdruple_queries/theme/products-page.tpl.php
+++ modules/xdruple_queries/theme/products-page.tpl.php
@@ -1,6 +1,13 @@
+<div class="subcategories-list">
+  <?php if (!empty($subcategories)): ?>
+    <h3><?php print t("Subcategories") ?></h3>
+    <?php print $subcategories; ?>
+  <?php endif; ?>
+</div>
 <div class="view-ft-commerce-xd-products">
   <div class="view-content">
     <?php if (!empty($columns)): ?>
+      <h3><?php print t("Products") ?></h3>
       <?php foreach ($columns as $column): ?>
         <div class="views-columns">
           <?php foreach ($column as $product): ?>
