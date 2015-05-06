4f5693c17081b31d823f378b267a60f6761d31e8
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-05-06 09:54:28 -0400

    Removed cart icon as cart button is not a dropdown

--- theme/xdruple-commerce-cart-dropdown.tpl.php
+++ theme/xdruple-commerce-cart-dropdown.tpl.php
@@ -1,12 +1,19 @@
+<?php
+/**
+ * @var string $link
+ * @var int    $count
+ * @var string $title
+ * @var string $amount
+ */
+?>
 <div class="btn-group">
   <a href="<?php print $link ?>" class="btn btn-primary">
     <?php if ($count == 0): ?>
       <div class="empty-cart"><span class="my-cart"><?php print $title ?></span></div>
     <?php else: ?>
-      <span class="my-cart"><?php print $title ?></span>
+      <span class="my-cart"><?php print $title ?>:</span>
       <span class="count"><?php print $count ?></span>
       <span class="amount"><?php print $amount ?></span>
-      <span class="caret"></span>
     <?php endif; ?>
   </a>
 </div>
