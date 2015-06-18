63037b5917fd441ebfe646b7dedcb333990ff33d
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-06-18 15:04:58 -0400

    Category: Documents handling added

--- src/Xtuple/XdrupleQueries/Categories/Category.php
+++ src/Xtuple/XdrupleQueries/Categories/Category.php
@@ -1,5 +1,7 @@
 <?php namespace Xtuple\XdrupleQueries\Categories;
 
+use Xtuple\REST\Resource\DocumentAssociation\DocumentAssociationList;
+
 /**
  * Class Category
  *
@@ -22,16 +24,21 @@ class Category {
   private $children = [];
   private $parents = [];
   private $path = [];
+  /** @var \Xtuple\REST\Resource\DocumentAssociation\DocumentAssociationList */
+  private $documents;
+  /** @var \stdClass[] */
+  private $files = [];
 
   /**
    * @param integer    $id
    * @param string     $name
    * @param Category[] $children
    */
-  public function __construct($id, $name, $children = []) {
+  public function __construct($id, $name, $children = [], $documents = []) {
     $this->setId($id);
     $this->setName($name);
     $this->setChildren($children);
+    $this->setDocuments($documents);
   }
 
   /**
@@ -124,5 +131,55 @@ class Category {
    */
   public function setName($name) {
     $this->name = $name;
+  }
+
+  /**
+   * @param array $documents
+   */
+  public function setDocuments(array $documents) {
+    $this->documents = DocumentAssociationList::constructFromResponseData($documents);
+    $this->files = $this->importFiles();
+  }
+
+  /**
+   * @return \Xtuple\REST\Resource\DocumentAssociation\DocumentAssociationList
+   */
+  public function getDocuments() {
+    return $this->documents;
+  }
+
+  /**
+   * @return \stdClass[]
+   */
+  public function files() {
+    return $this->files;
+  }
+
+  protected function importFiles() {
+    $files = [];
+    if (!empty($this->documents)) {
+      $dir = "public://content/external/xd-catalog/" . $this->getId();
+      file_prepare_directory($dir, FILE_MODIFY_PERMISSIONS | FILE_CREATE_DIRECTORY);
+      foreach ($this->documents as $document) {
+        if ($document->targetType() == "URL") {
+          if ($file = entity_uuid_load("file", [$document->uuid()])) {
+            $files[$document->uuid()] = array_pop($file);
+          }
+          else {
+            try {
+              if ($url = xdruple_fields_get_doc_url($document->target())) {
+                $file = system_retrieve_file($url["path"], $dir, $managed = TRUE);
+                $file->uuid = $document->uuid();
+                $file = file_save($file);
+                $files[$document->uuid()] = $file;
+              }
+            } catch (\Exception $e) {
+              watchdog_exception("xdruple_queries", $e);
+            }
+          }
+        }
+      }
+    }
+    return $files;
   }
 }
--- src/Xtuple/XdrupleQueries/Categories/Tree.php
+++ src/Xtuple/XdrupleQueries/Categories/Tree.php
@@ -90,7 +90,7 @@ class Tree {
     // getCatalog() called entity_load('xtuple_xdcatalog'), so entity_load_single()
     // will hit it's static cache instead of the REST API.
     $group = entity_load_single('xtuple_xdcatalog', $entityId);
-    $category = new Category($entityId, $group->name);
+    $category = new Category($entityId, $group->name, [], $group->documents);
     if (!empty($group->groups)) {
       foreach ($group->groups as $groupId) {
         $category->addChild(self::buildCategory($groupId));
