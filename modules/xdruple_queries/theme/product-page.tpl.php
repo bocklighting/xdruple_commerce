4dc5bc22a56e5841012a19d46e729b31ae7a3128
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-06-22 17:47:02 -0400

    Refactor: product page theming moved to own function

--- modules/xdruple_queries/theme/product-page.tpl.php
+++ modules/xdruple_queries/theme/product-page.tpl.php
@@ -2,10 +2,25 @@
 /**
  * @var string $classes
  * @var string $attributes
+ * @var array  $context
  */
 ?>
 <div class="<?php print $classes ?>"<?php print $attributes ?>>
-  <?php if (!empty($entity_view)): ?>
-    <?php print $entity_view ?>
-  <?php endif; ?>
+  <div class="row">
+    <div class="col-lg-8">
+      <?php if (!empty($entity)): ?>
+        <?php print drupal_render($entity) ?>
+      <?php endif; ?>
+    </div>
+    <div class="col-lg-4">
+      <?php if (!empty($context)): ?>
+        <?php print drupal_render($context) ?>
+      <?php endif; ?>
+    </div>
+    <?php if (!empty($bottom)): ?>
+      <div class="col-lg-12">
+        <?php print drupal_render($bottom); ?>
+      </div>
+    <?php endif; ?>
+  </div>
 </div>
