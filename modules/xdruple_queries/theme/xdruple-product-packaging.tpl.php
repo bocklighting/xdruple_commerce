3c83419845eeebab1e1468e08591a41a7a1060e4
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-06-30 18:02:50 -0400

    xdruple_product_packaging theming implemented

--- modules/xdruple_queries/theme/xdruple-product-packaging.tpl.php
+++ modules/xdruple_queries/theme/xdruple-product-packaging.tpl.php
@@ -2,10 +2,14 @@
 /**
  * @var string $classes
  * @var string $attributes
+ * @var array  $info
  */
 ?>
-<div class="<?php print $classes ?>"<?php print $attributes ?>>
-  <?php if (!empty($title)): ?>
-    <h4><?php print $title ?></h4>
-  <?php endif; ?>
-</div>
+<?php if (!empty($info)): ?>
+  <div class="<?php print $classes ?>"<?php print $attributes ?>>
+    <?php if (!empty($title)): ?>
+      <h4><?php print $title ?></h4>
+    <?php endif; ?>
+    <?php print drupal_render($info); ?>
+  </div>
+<?php endif; ?>
