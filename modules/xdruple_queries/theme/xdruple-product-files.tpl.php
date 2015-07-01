d1afe075ba320586c2f3b25ec4180758326e7ced
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-07-01 17:07:13 -0400

    Product files themed

--- modules/xdruple_queries/theme/xdruple-product-files.tpl.php
+++ modules/xdruple_queries/theme/xdruple-product-files.tpl.php
@@ -4,8 +4,19 @@
  * @var string $attributes
  */
 ?>
-<div class="<?php print $classes ?>"<?php print $attributes ?>>
-  <?php if (!empty($title)): ?>
-    <h4><?php print $title ?></h4>
-  <?php endif; ?>
-</div>
+<?php if (!empty($files)): ?>
+  <div class="<?php print $classes ?>"<?php print $attributes ?>>
+    <?php if (!empty($title)): ?>
+      <div class="panel-heading">
+        <h4 class="panel-title"><?php print $title ?></h4>
+      </div>
+    <?php endif; ?>
+    <ul class="list-group">
+      <?php foreach ($files as $file): ?>
+        <li class="list-group-item">
+          <?php print drupal_render($file); ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+<?php endif; ?>
