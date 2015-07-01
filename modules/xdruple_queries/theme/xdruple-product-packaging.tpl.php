ac0a9d6609b38fb4ed75003950d1d36397fa7f90
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-07-01 18:12:12 -0400

    Use panel to theme Product packaging

--- modules/xdruple_queries/theme/xdruple-product-packaging.tpl.php
+++ modules/xdruple_queries/theme/xdruple-product-packaging.tpl.php
@@ -8,7 +8,9 @@
 <?php if (!empty($info)): ?>
   <div class="<?php print $classes ?>"<?php print $attributes ?>>
     <?php if (!empty($title)): ?>
-      <h4><?php print $title ?></h4>
+      <div class="panel-heading">
+        <h4 class="panel-title"><?php print $title ?></h4>
+      </div>
     <?php endif; ?>
     <?php print drupal_render($info); ?>
   </div>
