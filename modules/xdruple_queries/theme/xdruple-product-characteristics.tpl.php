de9ddef9d7ddb24a32f9033e83c5e3448a7365cc
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-07-01 18:10:21 -0400

    Characteristics table theming added

--- modules/xdruple_queries/theme/xdruple-product-characteristics.tpl.php
+++ modules/xdruple_queries/theme/xdruple-product-characteristics.tpl.php
@@ -4,8 +4,13 @@
  * @var string $attributes
  */
 ?>
-<div class="<?php print $classes ?>"<?php print $attributes ?>>
-  <?php if (!empty($title)): ?>
-    <h4><?php print $title ?></h4>
-  <?php endif; ?>
-</div>
+<?php if (!empty($characteristics)): ?>
+  <div class="<?php print $classes ?>"<?php print $attributes ?>>
+    <?php if (!empty($title)): ?>
+      <div class="panel-heading">
+        <h4 class="panel-title"><?php print $title ?></h4>
+      </div>
+    <?php endif; ?>
+    <?php print drupal_render($characteristics_table); ?>
+  </div>
+<?php endif; ?>
