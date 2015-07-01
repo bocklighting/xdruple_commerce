046bce10194c43e14cb8443a51fbd1444918de24
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-07-01 16:48:56 -0400

    Product illustrations themed

--- modules/xdruple_queries/theme/xdruple-product-illustrations.tpl.php
+++ modules/xdruple_queries/theme/xdruple-product-illustrations.tpl.php
@@ -2,7 +2,13 @@
 /**
  * @var string $classes
  * @var string $attributes
+ * @var array  $images
  */
 ?>
-<div class="<?php print $classes ?>"<?php print $attributes ?>>
-</div>
+<?php if (!empty($images)): ?>
+  <div class="<?php print $classes ?>"<?php print $attributes ?>>
+    <?php foreach ($images as $image): ?>
+      <?php print drupal_render($image); ?>
+    <?php endforeach; ?>
+  </div>
+<?php endif; ?>
