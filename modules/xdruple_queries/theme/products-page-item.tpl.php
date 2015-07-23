92a170a76856d59ba1bb06ca71992a4aa146a189
Andrey Mikheychik <a.mikheychik@gmail.com>
2015-07-23 10:01:21 -0400

    Fix: products page button layout

--- modules/xdruple_queries/theme/products-page-item.tpl.php
+++ modules/xdruple_queries/theme/products-page-item.tpl.php
@@ -17,16 +17,12 @@
   </h4>
 <?php endif; ?>
 <?php if (!empty($read_more)): ?>
-  <div class="views-field views-field-nothing">
-  <span class="field-content">
+  <div class="xdruple-products-page-item-read-more">
     <?php print $read_more ?>
-  </span>
   </div>
 <?php endif; ?>
 <?php if (!empty($add_to_cart)): ?>
-  <div class="views-field views-field-add-to-cart-form">
-  <span class="field-content">
+  <div class="xdruple-products-page-item-add-to-cart">
     <?php print $add_to_cart ?>
-  </span>
   </div>
 <?php endif; ?>
